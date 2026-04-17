c4e487abd122e5b1af999cbc1d8d479d8b7b38f8
haizhou <haizhou_zhao@outlook.com>
2026-04-17 11:05:09 -0400

    Bump tracking ONNX exporter opset

--- src/mjlab/tasks/tracking/rl/exporter.py
+++ src/mjlab/tasks/tracking/rl/exporter.py
@@ -133,7 +133,7 @@ class _OnnxMotionPolicyExporter(_OnnxPolicyExporter):
       (obs, time_step),
       os.path.join(path, filename),
       export_params=True,
-      opset_version=11,
+      opset_version=18,
       verbose=self.verbose,
       input_names=["obs", "time_step"],
       output_names=output_names,
